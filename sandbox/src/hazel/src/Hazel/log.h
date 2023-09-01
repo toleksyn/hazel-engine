43a37a2d9751c006c6d4973997a5ffc382851aa7
Taras Oleksyn <taras.oleksyn@gmail.com>
2023-09-01 06:06:34 +0300

    toleksyn: define macros update

--- sandbox/src/hazel/src/Hazel/log.h
+++ sandbox/src/hazel/src/Hazel/log.h
@@ -17,16 +17,16 @@ namespace Hazel {
 }
 
 // core log macros
-#define HZ_CORE_ERROR(...)  ::Hazel::Log::getCoreLogger()->error(__VA_ARGS__)
-#define HZ_CORE_WARN(...)   ::Hazel::Log::getCoreLogger()->warn(__VA_ARGS__)
-#define HZ_CORE_INFO(...)   ::Hazel::Log::getCoreLogger()->info(__VA_ARGS__)
-#define HZ_CORE_TRACE(...)  ::Hazel::Log::getCoreLogger()->trace(__VA_ARGS__)
-#define HZ_CORE_FATAL(...)  ::Hazel::Log::getCoreLogger()->fatal(__VA_ARGS__)
+#define HZ_CORE_ERROR(...)  Hazel::Log::getCoreLogger()->error(__VA_ARGS__)
+#define HZ_CORE_WARN(...)   Hazel::Log::getCoreLogger()->warn(__VA_ARGS__)
+#define HZ_CORE_INFO(...)   Hazel::Log::getCoreLogger()->info(__VA_ARGS__)
+#define HZ_CORE_TRACE(...)  Hazel::Log::getCoreLogger()->trace(__VA_ARGS__)
+#define HZ_CORE_FATAL(...)  Hazel::Log::getCoreLogger()->fatal(__VA_ARGS__)
 
-// core log macros
-#define HZ_ERROR(...)  ::Hazel::Log::getClientLogger()->error(__VA_ARGS__)
-#define HZ_WARN(...)   ::Hazel::Log::getClientLogger()->warn(__VA_ARGS__)
-#define HZ_INFO(...)   ::Hazel::Log::getClientLogger()->info(__VA_ARGS__)
-#define HZ_TRACE(...)  ::Hazel::Log::getClientLogger()->trace(__VA_ARGS__)
-#define HZ_FATAL(...)  ::Hazel::Log::getClientLogger()->fatal(__VA_ARGS__)
+// client log macros
+#define HZ_ERROR(...)  Hazel::Log::getClientLogger()->error(__VA_ARGS__)
+#define HZ_WARN(...)   Hazel::Log::getClientLogger()->warn(__VA_ARGS__)
+#define HZ_INFO(...)   Hazel::Log::getClientLogger()->info(__VA_ARGS__)
+#define HZ_TRACE(...)  Hazel::Log::getClientLogger()->trace(__VA_ARGS__)
+#define HZ_FATAL(...)  Hazel::Log::getClientLogger()->fatal(__VA_ARGS__)
 
